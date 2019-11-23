50802f25b42f89ae8be2f9c645d1769ce9ee05a8
Jordan Whitten <jordanwa1947@gmail.com>
2019-11-22 18:26:27 -0700

    Remove margin on click so account details float to the top and add border to the nav image being visited

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var welcomeBanner = document.getElementById('welcome-banner');
 var closeWelcomeX = document.getElementById('close-welcome-x');
+var accountDetails = document.getElementById('account-details-cont');
+var centerColumn = document.getElementById('center-column');
 closeWelcomeX.addEventListener('click', closeWelcome);
 
 function closeWelcome () {
   welcomeBanner.style.display = 'none';
+  accountDetails.style.margin = '0px';
+  centerColumn.style.margin = '0px 34px 0px 0px';
 }
