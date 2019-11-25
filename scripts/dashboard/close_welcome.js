f798277eadc0f963f5212930b0ea0cc60d009ff2
Jordan Whitten <jordanwa1947@gmail.com>
2019-11-25 00:55:51 -0700

    Add styles to the stylesheet rather than as inline styles so theyll persist

--- scripts/dashboard/close_welcome.js
+++ scripts/dashboard/close_welcome.js
@@ -1,15 +1,16 @@
 export { closeWelcome }
 
 var closeWelcome = (function() {
-  var welcomeBanner = document.getElementById('welcome-banner');
   var closeWelcomeX = document.getElementById('close-welcome-x');
-  var accountDetails = document.getElementById('account-details-cont');
-  var centerColumn = document.getElementById('center-column');
 
   var makeWelcomeDisappear = function () {
-    welcomeBanner.style.display = 'none';
-    accountDetails.style.margin = '0px';
-    centerColumn.style.margin = '0px 34px 0px 0px';
+    // creates a style element, appends it to the head element 
+    // and inserts rules to the stylesheet so the styles persist
+    var style = document.createElement('style');
+    document.head.appendChild(style);
+    style.sheet.insertRule( `#welcome-banner { display: none; }`);
+    style.sheet.insertRule( `section#account-details-cont { margin: 0px; }`);
+    style.sheet.insertRule( `.center-column { margin: 0px 34px 0px 0px; }`);
   }
 
   function closeWelcomeOnClick () {
